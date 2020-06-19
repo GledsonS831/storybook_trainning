f1902aff88b50dca3ae21459ddc6a5df17bf0996
GLEDSONS831 <glegogle84@gmail.com>
2020-06-19 14:05:59 -0300

    :hammer: refactory project

---
+++ tests/src/Components/Buttom/Default/Buttom.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Default() {
+  return (
+    <button>
+      <h1>click</h1>
+    </button>
+  );
+}
---
+++ tests/src/Components/Buttom/buttom.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Default } from "./Default/Buttom";
+
+export default {
+  title: "Default",
+};
+
+export const comp = () => {
+  return <Default />;
+};
