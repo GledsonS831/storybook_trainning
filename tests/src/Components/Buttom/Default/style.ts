21f3e57ab3c25de3079ceaf9a03a9a85055385c6
GLEDSONS831 <glegogle84@gmail.com>
2020-06-20 03:37:50 -0300

    :hammer: refactoy

--- tests/src/Components/Buttom/Default/style.ts
+++ tests/src/Components/Buttom/Default/style.ts
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 export const container = styled.div`
   background-color: green;
-  box-shadow: 2px 2px solid black;
 `;
 
 export const content = styled.p`
