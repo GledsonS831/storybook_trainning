56c4bfade2697fd5fb0013e532d5fb4882aa5c7a
GledsonS831 <glegogle84@gmail.com>
2020-06-22 23:05:13 -0300

    :sparkles: add component buttom

---
+++ tests/src/stories/Components/Buttom/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as St from './style';
+
+
+
+export const Buttom = () =>{
+    return (
+        <St.Container>
+            <St.Content>
+                botao 1
+            </St.Content>
+        </St.Container>
+    )
+}
---
+++ tests/src/stories/Components/Buttom/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: rgb(110, 100, 100);
+    width: 80px;
+    height: 40;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 1s;
+    :hover{
+        width: 160px;
+        height: 80px;
+        background-color: rgb(60, 150, 30);
+    }
+`
+
+export const Content = styled.p`
+    font-size: 16px;
+    transition: 1s;
+    :hover{
+        color: white;
+        font-size: 24px;
+    }
+`
